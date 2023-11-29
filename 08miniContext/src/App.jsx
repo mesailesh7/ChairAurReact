e9df1e65fa8b73a9223a8a25d85867da5c0a842f
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2023-11-28 23:24:45 -0700

    Context API Discussion and Project

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import UserContextProvider from "./context/UserContextProvider";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+
+const App = () => {
+  return (
+    <UserContextProvider>
+      <h1>React with chai. Share is important</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  );
+};
+
+export default App;
